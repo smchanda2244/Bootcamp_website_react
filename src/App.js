bd96b976323adf92f3501f943bcb7cb05ed514c2
Sai Chanda <saichanda@MacBook-Pro-39.local>
2024-04-01 21:01:56 -0400

    Yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function PersonalWebsite() {
+    return (
+        <div>
+            <header>
+                <h1>Sai Chanda</h1>
+                <h2>Personal Website</h2>
+            </header>
+
+            <section>
+                <h2>About Me</h2>
+                <p>
+                    I am a computer science major at the University of Maryland: College Park, and love everything about it.
+                    I grew up in Northern Virginia and went to Thomas Jefferson High School for Science and Technology in Alexandria, VA.
+                    While currently looking for internships and more professional experiences in CS,
+                    I am working on many projects on my own time, with some of them being displayed on this website.
+                </p>
+            </section>
+
+            <section>
+                <h2>Interests</h2>
+                {}
+            </section>
+
+            <section>
+                <h2>Contact</h2>
+                <p>
+                    <br /><strong>Phone: 571-536-0140</strong>
+                    <br /><strong><a href="https://www.linkedin.com/in/sai-chanda/">Linkedin</a></strong>
+                    <br /><strong>Email: Smchanda2244@gmail.com</strong>
+                </p>
+            </section>
+
+            <footer>
+                &copy; 2024 Sai Chanda
+            </footer>
+        </div>
+    );
 }
 
-export default App;
+export default PersonalWebsite;
+
